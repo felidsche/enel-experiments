3b7aa88e8d439e9dcd7728e267a85a710a2df1f2
Feli dsche <felix@schneider.de>
2021-09-07 14:35:03 +0100

    tc, history server and kmeans

--- spark_utils/src/main/scala/de/tu_berlin/dos/arm/spark_utils/datagens/KMeansDataGenerator.scala
+++ spark_utils/src/main/scala/de/tu_berlin/dos/arm/spark_utils/datagens/KMeansDataGenerator.scala
@@ -1,23 +1,28 @@
 package de.tu_berlin.dos.arm.spark_utils.datagens
 
+/*
+ * Data generation for KMeans clustering
+ * writes to local filesystem
+ */
+
 import breeze.linalg.DenseVector
 import breeze.stats.distributions.Rand
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
 case class MeanConf(mean: DenseVector[Double], stdDev: Double)
+import java.io.{File, PrintWriter}
 
 object KMeansDataGenerator {
 
   def main(args: Array[String]): Unit = {
     if (args.length < 3) {
-      println("KMeansDataGenerator <samples> <cluster> <output> <defaultHdfsFs>")
+      println("KMeansDataGenerator <samples> <cluster> <output>")
       System.exit(1)
     }
-
+    /*
+    default conf: 555555 55 ../../../../samples/Kmeans.txt
+     */
     val n = args(0).toInt
     val k = args(1).toInt
     val outputPath = args(2)
-    val defaultFs = args(3)
 
     val dim = 2
     val stdDev = .012
@@ -27,21 +32,15 @@ object KMeansDataGenerator {
     val centers = uniformRandomCenters(dim, k, stdDev)
     val centerDistribution = Rand.choose(centers)
 
-    System.setProperty("HADOOP_USER_NAME", "drms")
-    val path = new Path(outputPath)
-    val conf = new Configuration()
-    conf.set("fs.defaultFS", defaultFs)
-    conf.set("fs.hdfs.impl", classOf[org.apache.hadoop.hdfs.DistributedFileSystem].getName);
-    conf.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName);
+    val file = new File(outputPath)
+    val pw = new PrintWriter(file)
 
-    val fs = FileSystem.get(conf)
-    val os = fs.create(path)
     (1 to n).foreach(_ => {
       val MeanConf(mean, stdDev) = centerDistribution.draw()
       val p = mean + DenseVector.rand[Double](mean.length, Rand.gaussian(0, stdDev))
-      os.writeBytes(p.toArray.mkString(" ") + "\n")
+      pw.write(p.toArray.mkString(" ") + "\n")
     })
-    fs.close()
+    pw.close()
 
   }
 
--- spark_utils/src/main/scala/de/tu_berlin/dos/arm/spark_utils/jobs/KMeans.scala
+++ spark_utils/src/main/scala/de/tu_berlin/dos/arm/spark_utils/jobs/KMeans.scala
@@ -1,15 +1,21 @@
-/*
- * KMeans workload for BigDataBench
- */
 package de.tu_berlin.dos.arm.spark_utils.jobs
 
+/*
+ * KMeans workload for experiments
+ */
+
+import org.apache.spark.mllib.clustering
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.{SparkConf, SparkContext}
 import org.rogach.scallop.exceptions.ScallopException
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import scala.concurrent.duration._
+
 object KMeans {
-  val MLLibKMeans = org.apache.spark.mllib.clustering.KMeans
+  val MLLibKMeans: clustering.KMeans.type = org.apache.spark.mllib.clustering.KMeans
 
   def main(args: Array[String]): Unit = {
 
@@ -19,23 +25,42 @@ object KMeans {
     val splits = 2
     val sparkConf = new SparkConf()
       .setAppName(appSignature)
+      .setMaster("local") // TODO: remove before cluter execution
+
+    val form = new SimpleDateFormat("dd.MM.yyyy_HH:MM:SS");
+    val execCal = Calendar.getInstance
+
+    val executionDate = form.format(execCal.getTime);
 
     val sparkContext = new SparkContext(sparkConf)
-    sparkContext.setCheckpointDir("../checkpoints")
+    sparkContext.setCheckpointDir("../../checkpoints/KMeans/" + executionDate + "/")
 
     println("Start KMeans training...")
     // Load and parse the data
     val data = sparkContext.textFile(conf.input(), splits)
     val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble)))
-    println("Start checkpointing...")
+
+    val checkpointStartCal =  Calendar.getInstance
+    val checkpointStartMillis = checkpointStartCal.getTimeInMillis
+    println("Start checkpoint at " + checkpointStartMillis.toString)
+
     // add a checkpoint on the map task to resume here if clustering fails
     parsedData.checkpoint()
 
+    val checkpointEndCal =  Calendar.getInstance
+    val checkpointEndMillis = checkpointEndCal.getTimeInMillis
+    println("End checkpoint at " + checkpointEndMillis.toString)
+
+    val checkpointDuration = Duration(checkpointEndMillis - checkpointStartMillis, MILLISECONDS)
+    println("Checkpoint duration " + checkpointDuration.toString)
+
+    //TODO: add a failure here
     val clusters = new org.apache.spark.mllib.clustering.KMeans()
       .setEpsilon(0)
       .setK(conf.k())
       .setMaxIterations(conf.iterations())
       .run(parsedData)
+
 
     // Evaluate clustering by computing Within Set Sum of Squared Errors
     val WSSSE = clusters.computeCost(parsedData)
@@ -68,4 +93,3 @@ class KMeansArgs(a: Seq[String]) extends ScallopConf(a) {
 
   verify()
 }
-
