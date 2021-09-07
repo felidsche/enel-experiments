6eac4cc86ecc6001cb8fe335519379ca5e659542
Feli dsche <felix@schneider.de>
2021-09-07 13:03:19 +0100

    finish get tc

--- service/src/experiment_metrics.py
+++ service/src/experiment_metrics.py
@@ -1,13 +1,27 @@
+import re
+
+
 class ExperimentMetrics:
 
-    def __init__(self):
-        return
+    def __init__(self, has_checkpoint: bool = False):
+        self.has_checkpoint = has_checkpoint
 
-    def get_tc(self) -> int:
+    def get_tc(self, log: str) -> int:
         """
         returns the time taken for checkpoints of a task in a job in ms
         """
-        return 0
+        pattern = r"(Checkpointing took\s)(\d{2,})"
+        tc = 0
+        count = 0
+        matches = re.finditer(pattern=pattern, string=log)
+        for match in matches:
+            count += 1
+            tc += int(match.group(2))
+        print(f"{count} checkpoints are in the log")
+        return tc
+
+    def get_has_checkpoint(self) -> bool:
+        return self.has_checkpoint
 
     def calc_mttr(self) -> int:
         """
--- service/test/test_experiment_metrics.py
+++ service/test/test_experiment_metrics.py
@@ -1,13 +1,31 @@
 from unittest import TestCase
+
 from src.experiment_metrics import ExperimentMetrics
+
+
+def get_log():
+    with open("fixtures/app.log", mode="r") as file:
+        return file.read()
+
 
 class TestExperimentMetrics(TestCase, ExperimentMetrics):
 
     def test_get_tc(self):
-        tc = self.get_tc()
-        self.assertTrue(tc == 0)
+        em = ExperimentMetrics(has_checkpoint=True)
+        tc = em.get_tc(log=get_log())
+        if em.get_has_checkpoint():
+            print(f"Tc: {tc}")
+            self.assertIsInstance(tc, int, "Tc is int")
+            self.assertGreater(tc, 0, f"TC: <= 0")
 
+    def test_get_tc_zero(self):
+        log = "Chejkpoint took: 952938 ms"
+        em = ExperimentMetrics(has_checkpoint=True)
+        tc = em.get_tc(log=log)
+        self.assertEqual(tc, 0)
 
     def test_calc_mttr(self):
         mttr = self.calc_mttr()
-        self.assertTrue(mttr == 0)
+        print(f"MTTR: {mttr}")
+        self.assertIsInstance(mttr, int, "MTTR is int")
+        self.assertGreater(mttr, 0, "MTTR <= 0")
