90278585d9b5036584a9bcf8a2d09238ad3619d9
Feli dsche <felix@schneider.de>
2021-09-10 09:44:08 +0200

    work on metrics request

--- service/src/experiment_metrics.py
+++ service/src/experiment_metrics.py
@@ -1,16 +1,44 @@
 import re
+import requests
+import json
 
 
 class ExperimentMetrics:
 
-    def __init__(self, has_checkpoint: bool = False):
+    def __init__(
+            self, has_checkpoint: bool = False,
+            hist_server_url: str = "http://localhost:18080/api/v1/"
+    ):
         self.has_checkpoint = has_checkpoint
+        self.hist_server_url = hist_server_url
+
+    def get_data_from_hist_server(self, hist_server_url: str, endpoint: str) -> dict:
+        data = {}
+        try:
+            data = json.loads(requests.get(url=hist_server_url + endpoint).content)
+        except json.JSONDecodeError as e:
+            print(f"{e} \n No values were returned")
+        return data
+
+    def get_job_data_from_hist_server(self, hist_server_url):
+
+        pass
+
+    def get_task_list_from_hist_server(self, hist_server_url, app_id: str, stage_id: str, stage_attempt_id: str):
+        endpoint = f"applications/{app_id}/stages/{stage_id}/{stage_attempt_id}/taskList"
+        task_list = self.get_data_from_hist_server(
+            hist_server_url=hist_server_url,
+            endpoint=endpoint
+        )
+
+
+        return task_list
 
     def get_tc(self, log: str) -> int:
         """
         returns the time taken for checkpoints of a task in a job in ms
         """
-        pattern = r"(Checkpointing took\s)(\d{2,})"
+        pattern = r"(Checkpointing took\s)(\d{2,})(\sms)"
         tc = 0
         count = 0
         matches = re.finditer(pattern=pattern, string=log)
--- service/test/test_experiment_metrics.py
+++ service/test/test_experiment_metrics.py
@@ -9,23 +9,49 @@ def get_log():
 
 
 class TestExperimentMetrics(TestCase, ExperimentMetrics):
+    em = ExperimentMetrics(has_checkpoint=True)
+
+    def test_get_data_from_hist_server(self):
+
+        jobs = self.em.get_data_from_hist_server(
+            hist_server_url=self.em.hist_server_url,
+            endpoint="applications"
+        )
+        self.assertIsInstance(jobs, list)
+
+    def test_get_task_list_from_hist_server(self):
+        task_list = self.em.get_task_list_from_hist_server(
+            hist_server_url=self.em.hist_server_url,
+            app_id="local-1631019386016",
+            stage_id="218",
+            stage_attempt_id="0"
+        )
+        self.fail()
 
     def test_get_tc(self):
-        em = ExperimentMetrics(has_checkpoint=True)
-        tc = em.get_tc(log=get_log())
-        if em.get_has_checkpoint():
+        if self.em.get_has_checkpoint():
+            tc = self.em.get_tc(log=get_log())
             print(f"Tc: {tc}")
             self.assertIsInstance(tc, int, "Tc is int")
             self.assertGreater(tc, 0, f"TC: <= 0")
 
     def test_get_tc_zero(self):
         log = "Chejkpoint took: 952938 ms"
-        em = ExperimentMetrics(has_checkpoint=True)
-        tc = em.get_tc(log=log)
-        self.assertEqual(tc, 0)
+        self.em = ExperimentMetrics(has_checkpoint=True)
+        if self.em.get_has_checkpoint():
+            tc = self.em.get_tc(log=log)
+            self.assertEqual(tc, 0)
+
+    def test_tc_not_ms(self):
+        # if tc is not in ms, then the regex does not match
+        log = "Checkpointing took: 952938 s"
+        if self.em.get_has_checkpoint():
+            tc = self.em.get_tc(log=log)
+            self.assertEqual(tc, 0)
 
     def test_calc_mttr(self):
         mttr = self.calc_mttr()
         print(f"MTTR: {mttr}")
         self.assertIsInstance(mttr, int, "MTTR is int")
         self.assertGreater(mttr, 0, "MTTR <= 0")
+
